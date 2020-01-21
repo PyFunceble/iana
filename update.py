d23c2de0e58fbd6ec3c23c5c730129e3789043ed
funilrys <contact@funilrys.com>
2020-01-21 22:02:25 +0100

    Update of the script.

--- update.py
+++ update.py
@@ -1,30 +1,91 @@
 #!/usr/bin/env python3
-import PyFunceble
+
+"""
+The tool to check the availability or syntax of domains, IPv4, IPv6 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+This script is part of the PyFunceble project.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.github.io/special-thanks.html
+
+Contributors:
+    https://pyfunceble.github.io/contributors.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://pyfunceble.github.io/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2019, 2020 PyFunceble
+    Copyright (c) 2017, 2018, 2019, 2020 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+
 import colorama
+import PyFunceble
+from PyFunceble.cli import Iana
 from PyFunceble.engine import ci
 
 if __name__ == "__main__":
 
     colorama.init(autoreset=True)
-    
+
     # We load our custom configuration
     CUSTOM_CONFIG = {
         "ci": True,
         "ci_autosave_final_commit": "Update iana-domains-db.json",
         "multiprocess": True,
         "maximal_processes": 50,
-        "dns_server": [
-            "one.one.one.one"
-        ]
+        "dns_server": ["one.one.one.one"],
     }
 
     PyFunceble.load_config(custom=CUSTOM_CONFIG)
-    # PyFunceble.cconfig.Preset() 
-    
+
     # We initiate the repostiory.
-    ci_engine = ci.TravisCI()
-    ci_engine.init() 
+    CI_ENGINE = ci.TravisCI()
+    CI_ENGINE.init()
 
-    PyFunceble.lookup.Iana().update()
+    Iana().update()
 
-    ci_engine.end_commit()
+    CI_ENGINE.end_commit()
